e92fff8eed83ffc885e1944de9b5eff0d71d9f1e
SamradhniS <72724885+SamradhniS@users.noreply.github.com>
2021-10-27 22:39:42 +0530

    Create BasicCalculation.cs
    hacktoberfest2021

---
+++ BasicCalculation.cs
@@ -0,0 +1,28 @@
+///C# Sharp program that takes two numbers as input and perform an operation (+,-,*,x,/) on them and displays the result of that operation.
+using System;
+public class BasicCalculation
+{
+    public static void Main()
+    {
+        int x, y;
+        char operation;
+         
+        Console.Write("Input first number: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input operation: ");
+        operation = Convert.ToChar(Console.ReadLine());
+        Console.Write("Input second number: ");
+        y = Convert.ToInt32(Console.ReadLine());
+         
+        if (operation=='+')
+            Console.WriteLine("{0} + {1} = {2}", x, y, x+y);
+        else if (operation=='-')
+            Console.WriteLine("{0} - {1} = {2}", x, y, x-y);
+        else if ((operation=='x') || (operation=='*'))
+            Console.WriteLine("{0} * {1} = {2}", x, y, x*y);
+        else if (operation=='/')
+            Console.WriteLine("{0} / {1} = {2}", x, y, x/y);
+        else
+            Console.WriteLine("Wrong Character");
+    }
+}
