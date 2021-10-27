dbd383d18aab27525a61c528e087a83fcc50a8cb
Manuradhni <73436621+Manuradhni@users.noreply.github.com>
2021-10-27 23:57:21 +0530

    Create 3dRadius.py

---
+++ 3dRadius.py
@@ -0,0 +1,5 @@
+Python program to get the volume of a sphere with radius from user.
+pi = 3.1415926535897931
+r=float(input("radius: ")) 
+V= 4.0/3.0*pi* r**3
+print('The volume of the sphere is: ',V)
